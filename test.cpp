48cc0409330a82c30a4535216619260bed356519
pateldhruv <pateld5@wit.edu>
2013-09-28 14:18:22 -0400

    d72 added add function

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+int add();
+
 int main ()
 {
 	for(int i=0; i <=10; i++)
@@ -8,4 +10,10 @@ int main ()
 		cout<<"Hello ";
 	}
 	return 0;
+}
+
+int add()
+{
+	int a=2, b=5;
+	return a+b;
 }
